ed66ef1b8a4554a4a9b508f7b17825926a0dc6b6
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2020-04-21 18:14:49 -0500

    Condiciones

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -1,11 +1,17 @@
 <script>
   //logica
 
-  let someText = `Estudiente de Platzi`;
+  let someText = "Estudiente de Platzi";
   let count = 0;
+  let style = { darkMode: false };
 
   function handleClick() {
     count += 1;
+  }
+
+  function toggle() {
+    style.darkMode = !style.darkMode;
+    window.document.body.classList.toggle("dark-mode");
   }
 </script>
 
@@ -16,6 +22,15 @@
 </style>
 
 <div class="About">
-  <p>{someText}</p>
+
+  {#if !style.darkMode}
+    <p>{someText}</p>
+  {:else}
+    <p>
+      <span>Hello DarckMode</span>
+    </p>
+  {/if}
+
   <button on:click={handleClick}>click {count === 0 ? '' : count}</button>
+  <button on:click={toggle}>DarkMode</button>
 </div>
